e77ec91d94e1ab8ff013eec828d096bcc0bc4d56
baldust <balder.klanderud@dengineering.no>
2020-02-20 15:51:55 +0100

    Added sensor data handling functions

---
+++ embedded_firmware/Inc/sensor_data.h
@@ -0,0 +1,20 @@
+//
+// Created by balder on 19.02.2020.
+//
+
+#ifndef EMBEDDED_FIRMWARE_SENSOR_DATA_H
+#define EMBEDDED_FIRMWARE_SENSOR_DATA_H
+
+#include "relu_1_none.h"
+
+typedef enum __sensor_data_source_t {
+    SENSOR_DATA_TEST, SENSOR_DATA_TRAIN
+} sensor_data_source_t;
+
+void get_sensor_values(double values[AI_RELU_1_NONE_OUT_1_SIZE]);
+
+void get_sensor_reading(double reading[AI_RELU_1_NONE_IN_1_SIZE]);
+
+void new_sensor_reading(sensor_data_source_t source);
+
+#endif //EMBEDDED_FIRMWARE_SENSOR_DATA_H
---
+++ embedded_firmware/Src/sensor_data.c
@@ -0,0 +1,40 @@
+//
+// Created by balder on 19.02.2020.
+//
+
+#include "sensor_data.h"
+#include "data/test_target.h"
+#include "data/train_target.h"
+#include "data/test_features.h"
+#include "data/train_features.h"
+#include <stdint.h>
+#include <string.h>
+
+static double current_reading[TEST_FEATURES_NUM_COLS];
+static double current_values[TEST_TARGET_NUM_COLS];
+
+void get_sensor_reading(double reading[TEST_FEATURES_NUM_COLS]) {
+    memcpy(reading, current_reading, sizeof(current_reading));
+}
+
+void get_sensor_values(double values[TEST_TARGET_NUM_COLS]) {
+    memcpy(values, current_values, sizeof(current_values));
+}
+
+void new_sensor_reading(sensor_data_source_t source) {
+    static uint16_t test_i = 0, train_i = 0;
+    switch (source) {
+        case SENSOR_DATA_TEST:
+            memcpy(current_reading, test_features[test_i], sizeof(current_reading));
+            memcpy(current_values, test_target[test_i], sizeof(current_values));
+            test_i = (test_i + 1) % TEST_FEATURES_NUM_ROWS;
+            break;
+        case SENSOR_DATA_TRAIN:
+            memcpy(current_reading, train_features[train_i], sizeof(current_reading));
+            memcpy(current_values, train_target[train_i], sizeof(current_values));
+            train_i = (train_i + 1) % TRAIN_FEATURES_NUM_ROWS;
+            break;
+        default:
+            break;
+    }
+}
